e34cc0eee126c4002c7d5117aca418edccdbc6ac
zahraAfa <anisafatimahazzahra@gmail.com>
2022-04-15 02:08:25 +0700

    latitude and longitude coordinates translated into an address

--- lib/app/modules/customer/modules/home/controllers/home_controller.dart
+++ lib/app/modules/customer/modules/home/controllers/home_controller.dart
@@ -1,8 +1,11 @@
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 Position? custCurrentPosition;
 var geoLocator = Geolocator();
+List<Placemark>? placeMarks;
+String custCompleteAddress = '';
 
 GoogleMapController? newGoogleMapController;
 
@@ -44,8 +47,28 @@ locateCustPosition() async {
   LatLng latLngPosition =
       LatLng(custCurrentPosition!.latitude, custCurrentPosition!.longitude);
 
+// To get the address
+  placemarkFromCoordinates(
+          custCurrentPosition!.latitude, custCurrentPosition!.longitude)
+      .then((pm) {
+    print('No results found.');
+    if (pm.isNotEmpty) {
+      print(pm[0].toString());
+      custCompleteAddress =
+          '${pm[0].street}, ${pm[0].subLocality} ${pm[0].locality}, ${pm[0].administrativeArea} ${pm[0].postalCode}, ${pm[0].country}';
+      print(custCompleteAddress);
+    }
+  });
+
+  // Placemark custPMark = placeMarks![0];
+
+  // String custCompleteAddress =
+  //     '${custPMark.subThoroughfare} ${custPMark.thoroughfare}, ${custPMark.subLocality} ${custPMark.locality}, ${custPMark.subAdministrativeArea}, ${custPMark.administrativeArea} ${custPMark.postalCode}, ${custPMark.country}';
+
+  // print(custCompleteAddress);
+
   CameraPosition cameraPosition =
-      CameraPosition(target: latLngPosition, zoom: 16);
+      CameraPosition(target: latLngPosition, zoom: 18);
 
   newGoogleMapController!
       .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
