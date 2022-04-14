944f14c92df4f74f923f5d6a7b2ef480343281fb
zahraAfa <anisafatimahazzahra@gmail.com>
2022-04-15 02:31:48 +0700

    technician raw home tab added

--- lib/app/modules/technician/modules/home/views/home_tab.dart
+++ lib/app/modules/technician/modules/home/views/home_tab.dart
@@ -19,19 +19,76 @@ class _HomeTabPageState extends State<HomeTabPage> {
     zoom: 14.4746,
   );
 
+  List<String> techStatus = ["Available", "Offline"];
+  String? selectedTechStatus;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
+      body: Column(
         children: [
-          GoogleMap(
-            initialCameraPosition: _kGooglePlex,
-            mapType: MapType.normal,
-            myLocationButtonEnabled: true,
-            onMapCreated: (GoogleMapController controller) {
-              _gMapController.complete(controller);
-              newGoogleMapController = controller;
+          const Text('Hi NAME,'),
+          const Text('where are you now?'),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 250,
+            child: GoogleMap(
+              initialCameraPosition: _kGooglePlex,
+              mapType: MapType.normal,
+              myLocationButtonEnabled: true,
+              onMapCreated: (GoogleMapController controller) {
+                _gMapController.complete(controller);
+                newGoogleMapController = controller;
+              },
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            'Jl. Duku No. 43, Bogor Timur Bogor, West Java 16143, Indonesia?',
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ElevatedButton.icon(
+            onPressed: () {},
+            icon: const Icon(Icons.location_on),
+            label: const Text('Change current location'),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          const Text('Status'),
+          const SizedBox(
+            height: 10,
+          ),
+          DropdownButton(
+            items: techStatus.map((category) {
+              return DropdownMenuItem(
+                child: Text(
+                  category,
+                  textAlign: TextAlign.center,
+                ),
+                value: category,
+              );
+            }).toList(),
+            onChanged: (newValue) {
+              setState(() {
+                selectedTechStatus = newValue.toString();
+              });
             },
+            value: selectedTechStatus,
+            hint: const Text(
+              "Please Choose status",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.grey,
+              ),
+            ),
           ),
         ],
       ),
