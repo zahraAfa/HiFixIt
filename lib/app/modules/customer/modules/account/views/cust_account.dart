a59c378cc62c4c49aa83e520d4f6de0dcd7677ba
zahraAfa <anisafatimahazzahra@gmail.com>
2022-04-23 15:44:40 +0700

    stream builder added

--- lib/app/modules/customer/modules/account/views/cust_account.dart
+++ lib/app/modules/customer/modules/account/views/cust_account.dart
@@ -17,20 +17,20 @@ class CustAccount extends StatefulWidget {
 class _CustAccountState extends State<CustAccount> {
   final TextEditingController firstNameC = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  DocumentSnapshot? _userInfo;
+  // DocumentSnapshot? _userInfo;
 
-  getCustData() async {
-    _userInfo = await FirebaseFirestore.instance
-        .collection("Customer")
-        .doc(currentFirebaseUser!.uid)
-        .get();
-  }
+  // Future getCustData() async {
+  //   _userInfo = await FirebaseFirestore.instance
+  //       .collection("Customer")
+  //       .doc(currentFirebaseUser!.uid)
+  //       .get();
+  // }
 
-  @override
-  void initState() {
-    super.initState();
-    getCustData();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getCustData();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -42,103 +42,117 @@ class _CustAccountState extends State<CustAccount> {
         title: const Text("Account"),
       ),
       body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              InkWell(
-                child: CircleAvatar(
-                  radius: MediaQuery.of(context).size.width * 0.20,
-                  backgroundColor: Colors.white,
-                  backgroundImage: _userInfo!["custPicture"].isEmpty
-                      ? null
-                      : NetworkImage(_userInfo!["custPicture"].toString()),
-                  child: _userInfo!["custPicture"].isNotEmpty
-                      ? null
-                      : Icon(
-                          Icons.person,
-                          size: MediaQuery.of(context).size.width * 0.20,
-                          color: const Color(0xFFBF84B1),
-                        ),
-                ),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              Container(
+        child: StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+            stream: FirebaseFirestore.instance
+                .collection("Customer")
+                .doc(currentFirebaseUser!.uid)
+                .snapshots(),
+            builder: (context, snapshot) {
+              return Container(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 20.0, horizontal: 50.0),
+                width: MediaQuery.of(context).size.width,
                 child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Text(
-                        "First Name",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(color: Colors.grey.shade600),
+                    InkWell(
+                      child: CircleAvatar(
+                        radius: MediaQuery.of(context).size.width * 0.20,
+                        backgroundColor: Colors.white,
+                        backgroundImage: snapshot.data!['custPicture'].isEmpty
+                            ? null
+                            : NetworkImage(
+                                snapshot.data!['custPicture'].toString()),
+                        child: snapshot.data!['custPicture'].isNotEmpty
+                            ? null
+                            : Icon(
+                                Icons.person,
+                                size: MediaQuery.of(context).size.width * 0.20,
+                                color: const Color(0xFFBF84B1),
+                              ),
                       ),
                     ),
-                    AccountInputCust(
-                      onChanged: (value) {},
-                      hintTitle: _userInfo!["custFName"] ?? "",
-                      keyboardType: TextInputType.text,
+                    const SizedBox(
+                      height: 30.0,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Text(
-                        "Last Name",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(color: Colors.grey.shade600),
+                    Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Text(
+                              "First Name",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          ),
+                          AccountInputCust(
+                            onChanged: (value) {},
+                            // hintTitle: _userInfo!["custFName"] ?? "",
+                            hintTitle: snapshot.data!['custFName'].toString(),
+                            keyboardType: TextInputType.text,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Text(
+                              "Last Name",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          ),
+                          AccountInputCust(
+                            onChanged: (value) {},
+                            hintTitle: snapshot.data!['custLName'].toString(),
+                            keyboardType: TextInputType.text,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Text(
+                              "Email",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          ),
+                          AccountInputCust(
+                            onChanged: (value) {},
+                            hintTitle: snapshot.data!['custEmail'].toString(),
+                            keyboardType: TextInputType.text,
+                            enabled: false,
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Text(
+                              "Phone No.",
+                              textAlign: TextAlign.start,
+                              style: TextStyle(color: Colors.grey.shade600),
+                            ),
+                          ),
+                          AccountInputCust(
+                            onChanged: (value) {},
+                            hintTitle: snapshot.data!['custPhone'].toString(),
+                            keyboardType: TextInputType.text,
+                          ),
+                        ],
                       ),
                     ),
-                    AccountInputCust(
-                      onChanged: (value) {},
-                      hintTitle: _userInfo!["custLName"] ?? "",
-                      keyboardType: TextInputType.text,
+                    const SizedBox(
+                      height: 30,
                     ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Text(
-                        "Email",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(color: Colors.grey.shade600),
+                    SizedBox(
+                      height: 40,
+                      child: AccountEditBtn(
+                        label: 'Save Changes',
+                        press: () {},
                       ),
-                    ),
-                    AccountInputCust(
-                      onChanged: (value) {},
-                      hintTitle: _userInfo!["custEmail"] ?? "",
-                      keyboardType: TextInputType.text,
-                      enabled: false,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Text(
-                        "Phone No.",
-                        textAlign: TextAlign.start,
-                        style: TextStyle(color: Colors.grey.shade600),
-                      ),
-                    ),
-                    AccountInputCust(
-                      onChanged: (value) {},
-                      hintTitle: _userInfo!["custPhone"] ?? "",
-                      keyboardType: TextInputType.text,
                     ),
                   ],
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SizedBox(
-                height: 40,
-                child: AccountEditBtn(
-                  label: 'Save Changes',
-                  press: () {},
-                ),
-              ),
-            ],
-          ),
-        ),
+              );
+            }),
       ),
     );
   }
